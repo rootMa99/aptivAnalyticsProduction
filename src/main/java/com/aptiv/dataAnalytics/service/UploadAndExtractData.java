c2836487e9599773068219323ae249e2f196f089
admin <anasszeroual09@gmail.com>
2023-12-01 12:50:59 +0100

    fc

--- src/main/java/com/aptiv/dataAnalytics/service/UploadAndExtractData.java
+++ src/main/java/com/aptiv/dataAnalytics/service/UploadAndExtractData.java
@@ -26,7 +26,7 @@ public class UploadAndExtractData {
 
     public static List<DataExcel> getDataFromEXcelFile(InputStream inputStream) {
 
-        List<DataExcel> dataExcels=new ArrayList<>();
+        List<DataExcel> dataExcels = new ArrayList<>();
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
             XSSFSheet sheet = workbook.getSheet("DATA");
@@ -39,288 +39,165 @@ public class UploadAndExtractData {
 
                 Iterator<Cell> cellIterator = row.iterator();
                 int cellIndex = 0;
-                DataExcel dataExcel=new DataExcel();
-                DataTargetExcel dataTargetExcel=new DataTargetExcel();
-                ActualDataExcel actualDataExcel=new ActualDataExcel();
-                System.out.println(rowIndex);
+                DataExcel dataExcel = new DataExcel();
+                DataTargetExcel dataTargetExcel = new DataTargetExcel();
+                ActualDataExcel actualDataExcel = new ActualDataExcel();
                 while (cellIterator.hasNext()) {
                     //Cell cell = cellIterator.next();
                     Cell cell = row.getCell(cellIndex, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                    System.out.println(cellIndex);
                     switch (cellIndex) {
                         case 0 -> {
-                            System.out.println(cell.getCellType());
-                            if (cell.getCellType() == CellType.STRING) {
-                                System.out.println("plant ######### " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("plant " + cell.getCellType());
-                            }
                         }
                         case 1 -> {
-                            System.out.println(cell.getCellType());
-                            if (cell.getCellType() == CellType.STRING) {
-                                System.out.println("area " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("area " + cell.getCellType());
-                            }
                         }
                         case 2 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING) {
                                 dataExcel.setProject(cell.getStringCellValue());
-                                System.out.println("project " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("project " + cell.getCellType());
                             }
                         }
                         case 3 -> {
                             System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING) {
                                 dataExcel.setFamily(cell.getStringCellValue());
-                                System.out.println("family " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("family " + cell.getCellType());
                             }
                         }
                         case 4 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING) {
                                 dataExcel.setCrew(cell.getStringCellValue());
-                                System.out.println("crew " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("crew " + cell.getCellType());
                             }
                         }
                         case 5 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING && cell.getCellType() != CellType.BLANK) {
                                 dataExcel.setTeamLeader(cell.getStringCellValue());
-                                System.out.println("team leader " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("team leader " + cell.getCellType());
                             }
                         }
                         case 6 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING && cell.getCellType() != CellType.BLANK) {
                                 dataExcel.setShiftLeader(cell.getStringCellValue());
-                                System.out.println("shift leader " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("shift leader " + cell.getCellType());
                             }
                         }
                         case 7 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING && cell.getCellType() != CellType.BLANK) {
                                 dataExcel.setCoordinator(cell.getStringCellValue());
-                                System.out.println("coordinator " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("coordinator " + cell.getCellType());
                             }
                         }
                         case 8 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING) {
                                 dataExcel.setMonth(cell.getStringCellValue());
-                                System.out.println("month " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("month " + cell.getCellType());
                             }
                         }
                         case 9 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.STRING) {
                                 dataExcel.setWeek(cell.getStringCellValue());
-                                System.out.println("week " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("week " + cell.getCellType());
                             }
                         }
                         case 10 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataExcel.setDate(cell.getDateCellValue());
-                                System.out.println("date " + cell.getDateCellValue());
-                            } else {
-                                System.out.println("date " + cell.getCellType());
                             }
                         }
                         case 11 -> {
-                            System.out.println(cell.getCellType());
-                            if (cell.getCellType() == CellType.STRING) {
-                                System.out.println("filter " + cell.getStringCellValue());
-                            } else {
-                                System.out.println("filter " + cell.getCellType());
-                            }
                         }
                         case 12 -> {
-                            System.out.println(cell.getCellType());
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setOutput((int) cell.getNumericCellValue());
-                                System.out.println("output " + cell.getNumericCellValue());
 
-                            } else {
-                                System.out.println("output " + cell.getCellType());
                             }
                         }
                         case 13 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setProdH(cell.getNumericCellValue());
-                                System.out.println("prod-h " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("prod-h " + cell.getCellType());
                             }
                         }
                         case 14 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setPaidH(cell.getNumericCellValue());
-                                System.out.println("paid-h " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("paid-h " + cell.getCellType());
                             }
                         }
                         case 15 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setTotalhc(cell.getNumericCellValue());
-                                System.out.println("totalHc " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("totalHc " + cell.getCellType());
                             }
                         }
                         case 16 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setHc(cell.getNumericCellValue());
-                                System.out.println("hc " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("hc " + cell.getCellType());
                             }
                         }
                         case 17 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setOt(cell.getNumericCellValue());
-                                System.out.println("ot " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("ot " + cell.getCellType());
                             }
                         }
                         case 18 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setAb(cell.getNumericCellValue());
-                                System.out.println("ab " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("ab " + cell.getCellType());
                             }
                         }
                         case 19 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setTlo(cell.getNumericCellValue());
-                                System.out.println("tlo " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("tlo " + cell.getCellType());
                             }
                         }
                         case 20 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 actualDataExcel.setDt(cell.getNumericCellValue());
-                                System.out.println("dt " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("dt " + cell.getCellType());
                             }
                         }
                         case 21 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setOutputTarget(cell.getNumericCellValue());
-                                System.out.println("output target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("output target " + cell.getCellType());
                             }
                         }
                         case 22 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setProdTarget(cell.getNumericCellValue());
-                                System.out.println("prod target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("prod target " + cell.getCellType());
                             }
                         }
                         case 23 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setPayedTarget(cell.getNumericCellValue());
-                                System.out.println("payed target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("payed target " + cell.getCellType());
                             }
                         }
                         case 24 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setHcTarget(cell.getNumericCellValue());
-                                System.out.println("hc target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("hc target " + cell.getCellType());
                             }
                         }
                         case 25 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setAbsTarget(cell.getNumericCellValue());
-                                System.out.println("abs target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("abs target " + cell.getCellType());
                             }
                         }
                         case 26 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setDtTarget(cell.getNumericCellValue());
-                                System.out.println("dt target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("dt target " + cell.getCellType());
                             }
                         }
                         case 27 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setScrap(cell.getNumericCellValue());
-                                System.out.println("scrap " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("scrap " + cell.getCellType());
                             }
                         }
                         case 28 -> {
-                            System.out.println(cell.getCellType());
 
                             if (cell.getCellType() == CellType.NUMERIC) {
                                 dataTargetExcel.setScrapTarget(cell.getNumericCellValue());
-                                System.out.println("scrap target " + cell.getNumericCellValue());
-                            } else {
-                                System.out.println("scrap target " + cell.getCellType());
                             }
                         }
                         default -> {
